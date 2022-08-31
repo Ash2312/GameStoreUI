0a4df7b943d5c48f7b930d1edcfd0fd33886bfa5
Ashmit <ashmitchugh3@gmail.com>
2022-08-31 09:55:57 +0530

    Added slide image functionality

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import '../data.dart';
+import 'package:gamify_app/data.dart';
+import '';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -9,13 +10,41 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  var _deviceHeight;
+  var _deviceWidth;
+  @override
+  void initState(){
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
+    _deviceHeight=MediaQuery.of(context).size.height;
+    _deviceWidth=MediaQuery.of(context).size.width;
     return Scaffold(
-      body: 
-      Container(
-        
-      )
-      );
+      body:Stack(children: <Widget>[
+        _featuredGamesWidget()
+
+      ],
+
+      ) 
+    
+    );
+  }
+  Widget _featuredGamesWidget(){
+    return SizedBox(height:_deviceHeight*0.5,width:_deviceWidth,
+    child: PageView(
+      scrollDirection: Axis.horizontal,
+      children: featuredGames.map((_game){
+        return Container(
+          decoration: 
+    BoxDecoration(
+      image: DecorationImage(
+        fit: BoxFit.cover
+        ,image: 
+      NetworkImage(_game.coverImage.url))
+    )
+        );
+      }).toList(),),
+    );
   }
 }
